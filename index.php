90ba78cd4dbbee4635042cb8ad6e4fdadcff4257
jimmy annes <jimmyannes98@gmail.com>
2014-12-03 14:04:43 -0800

    blog milestone 16

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
         <?php
-            require_once (__DIR__ . "/controller/create-db.php");
-            //links to the create db page for information
+            require_once (__DIR__ . "/view/header.php");//calls header to be used when printing out on screen
+            require_once (__DIR__ . "/controller/create-db.php");//links to the create db page for information
+            require_once (__DIR__ . "/view/footer.php");//calls footer to be used when printing out on screen
         ?>
-    </body>
-</html>
+
